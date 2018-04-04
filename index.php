c2d27188e3d2fe1d72ec99e5c08398b45631cb05
Kaila Funaki <yfunaki@csumb.edu>
2018-04-04 05:57:04 +0000

    adrian main page

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
+<?php 
+include '../dbConnection.php';
+$dbConn = getDatabaseConnection("otterstyle");
 
+function display(){
+    global $dbConn;
+
+    $sql = "SELECT * FROM `product` WHERE 1";
+		
+    $stmt = $dbConn->query($sql);	
+    $stmt->execute();
+    $records = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    
+    
+     echo "<table border=1 id='customer'>";
+        echo "<tr>";
+            echo "<th>Item Name</th>";
+            echo "<th>Price</td>";
+            echo "<th>More Info</th>";
+            echo "<th>Checkout</th>";
+            echo "</tr>";
+    
+    foreach($records as $record){
+        echo "<tr>";
+        echo "<td>". $record['productName']. "</td> ";
+        echo "<td>". $record['productPrice'] . "</td>";
+        
+        echo "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    
+}
+?>	 
 
 <!DOCTYPE html>
 <html>
@@ -6,6 +39,7 @@
         <title> </title>
     </head>
     <body>
+        <?=display(); ?>
 
     </body>
 </html>
